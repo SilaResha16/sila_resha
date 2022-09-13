6e2cfd1862cc15493de265590010dd3b7873bfa0
SilaResha16 <113013769+SilaResha16@users.noreply.github.com>
2022-09-13 06:06:21 -0700

    Add files via upload
    tugas 1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Sila Resha Nugraha" << endl;
+    cout << "A11.2022.14605" << endl;
+    cout << "Prodi Informatika" << endl;
+    cout << "Fakultas Ilmu Komputer" << endl;
+    cout << "111202214605@mhs.dinus.ac.id" << endl;
+    cout << "Gg.Mekarsari Indah RT:06/RW:02,Ds.Kertasari,Banjarharjo,Brebes" << endl;
+    cout << "Brebes,09 Agustus 2004" << endl;
+    cout << "Aku anak pertama dari dua bersaudara" << endl;
+    cout << "Nama adikku Pandu Bima Brata" << endl;
+    cout << "Aku tinggal di daerah perbatasan antara Jawa Tengah dan Jawa Barat" << endl;
+    cout << "Bahasa keseharianku bahasa sunda" << endl;
+    cout << "Asal sekolahku SMA N 2 Brebes" << endl;
+    cout << "Hobbyku memasak" << endl;
+    return 0;
+}
+
